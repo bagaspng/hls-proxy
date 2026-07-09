6319da22560dab34a9cb9a9b63edebd1a1f7c771
Bagas Pangestu <bagaspangestu0407@gmail.com>
2026-07-09 13:17:24 +0700

    Update STREAM_URL and REFERER in config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 STREAM_URL = (
-    "https://vault-13.owocdn.top/stream/13/14/da333978e2c3792eec006d08869128b9df1508803fa02869be540a45ef801576/uwu.m3u8"
+    "https://vault-13.owocdn.top/stream/.../uwu.m3u8"
 )
 
-REFERER = "https://kwik.cx/"
+REFERER = "https:(cdn)"
 
 # Server settings
 HOST = "127.0.0.1"
